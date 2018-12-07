1ec146700947c2b658be88e299226263869f76a1
arwill <awillis4@mercymavericks.edu>
2018-12-07 00:27:06 -0500

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n%ht+-gds-&hvq-&vd^r-j06rntp_3-d9tp5k0n-g3tn5&e%9_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
